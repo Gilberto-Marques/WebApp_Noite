8664178e65c12ef07c0b22025211b62bc1aec583
Gilberto Dias Marques Junior <gilberto.marques@fatec.sp.gov.br>
2023-03-24 20:32:19 -0300

    Criado uma tela inicial com imagens para teste de html.

--- WebApp_Noite/Controllers/HomeController.cs
+++ WebApp_Noite/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebApp_Noite.Controllers
             return View();
         }
 
+        public IActionResult MinhaTela()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
