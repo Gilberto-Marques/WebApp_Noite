79a5bf36ef380dbddf576123f613f63c6f070ee1
Gilberto Dias Marques Junior <gilberto.marques@fatec.sp.gov.br>
2023-03-31 20:00:27 -0300

    Criação do formulário de cadastro de produtos.

---
+++ WebApp_Noite/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+            //return view retorna a tela ao usuário, nesse caso, a tela cadastrar
+        }
+    }
+}
