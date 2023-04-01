e2a21bb6127d102e204347b6c9a7615dde8edff6
Gilberto-Marques <gilberto.marques@fatec.sp.gov.br>
2023-03-31 21:52:46 -0300

    Criado o Controller Cliente

---
+++ WebApp_Noite/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
