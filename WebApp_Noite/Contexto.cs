89b5144e3db19c6c8c82fda99beab6f3f4d645b9
Gilberto-Marques <gilberto.marques@fatec.sp.gov.br>
2023-05-12 21:10:52 -0300

    Incluído classe Contexto e alterado a classe Program e Adicionado dependências para interação do Visual Studio com o SQL Server.

---
+++ WebApp_Noite/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp_Noite
+{
+    public class Contexto : DbContext
+    {
+        public Contexto (DbContextOptions<Contexto> opt)
+            :base(opt){ }
+    }
+}
